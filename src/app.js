dd400ea4f5a945e90c251e357fd08afe4c05f441
misspia <pia_leung@hotmail.com>
2017-10-14 11:29:20 -0400

    hellow world test server

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('supp');
+})
+
+app.listen(PORT, () => console.log('Listening on port ', PORT));
