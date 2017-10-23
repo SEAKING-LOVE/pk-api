1b77807cc3c95164b1863f88cc6ee26bd35ccc66
misspia <pia_leung@hotmail.com>
2017-10-23 19:36:04 -0400

    init abilities controller, router, model

---
+++ src/controllers/abilities.controller.js
@@ -0,0 +1,17 @@
+const Abilities = require('../models/abilities.model.js');
+
+const Controller = {
+	all: (req, res) => {
+		Abilities.all()
+		.then((data) => res.send(data))
+		.catch((err) => res.send(err));
+	},
+	id: (req, res) => {
+		Abilities.id(req.params.id)
+		.then((data) => res.send(data))
+		.catch((err) => res.send(err));
+	}
+
+};
+
+module.exports = Controller;
---
+++ src/models/abilities.model.js
@@ -0,0 +1,13 @@
+const squel = require('squel');
+const query = require('../pgconnect.js');
+
+const Model = {
+	all: () => {
+
+	},
+	id: (id) => {
+
+	}
+}
+
+module.exports = Model;
