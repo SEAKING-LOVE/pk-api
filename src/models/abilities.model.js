6c0b638a44d918291bc78c27db19b0bd83ec00e6
misspia <pia_leung@hotmail.com>
2017-10-26 22:14:25 -0400

    filter using distinct attempt

--- src/models/abilities.model.js
+++ src/models/abilities.model.js
@@ -8,14 +8,14 @@ const tables = {
 const Model = {
 	all: () => {
 		const queryString = squel.select()
+			.distinct(`${tables.abilitiesDesc}.flavor_text`)
 			.from(tables.abilities)
 			.field(`${tables.abilities}.id`)
 			.field(`${tables.abilities}.identifier`)
 			.field(`${tables.abilitiesDesc}.flavor_text`)
-			// .field(`max(${tables.abilitiesDesc}.version_group_id)`)
+			.distinct() 	// figure out how to get distinc to work :S
 			.join(tables.abilitiesDesc, null, `${tables.abilities}.id = ${tables.abilitiesDesc}.ability_id`)
 			.where(`${tables.abilitiesDesc}.language_id = 9`)
-			// .group(`${tables.abilitiesDesc}.ability_id`)
 			.order(`${tables.abilities}.identifier`)
 			.toString();
 		return query(queryString)
