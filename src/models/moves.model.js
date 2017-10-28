5c6d98ead2f5c113aad336734276d76addc39b97
misspia <pia_leung@hotmail.com>
2017-10-28 12:47:04 -0400

    all moves: name, pp, power, acc, type

--- src/models/moves.model.js
+++ src/models/moves.model.js
@@ -2,17 +2,30 @@ const squel = require('squel');
 const query = require('../pgconnect.js');
 
 const tables = {
-	moves: 'moves',
-	moveCategory: 'move_battle_styles',
-	moveDetails: 'move_meta',
-	moveDesc: 'move_flavor_text',
-	types: 'types',
+	moves: 'pokemon.moves',
+	moveCategory: 'pokemon.move_battle_styles',
+	moveDetails: 'pokemon.move_meta',
+	moveDesc: 'pokemon.move_flavor_text',
+	types: 'pokemon.types',
 };
 
 
 const Model = {
 	all: () => {
-
+		const queryString = squel.select()
+			.from(tables.moves)
+			.field(`${tables.moves}.id`)
+			.field(`${tables.moves}.identifier`)
+			.field(`${tables.moves}.power`)
+			.field(`${tables.moves}.pp`)
+			.field(`${tables.moves}.accuracy`)
+			.field(`${tables.types}.identifier`, 'type')
+			.join(tables.types, null, `${tables.types}.id = ${tables.moves}.type_id`)
+			.order(`${tables.moves}.identifier`)
+			.toString();
+		return query(queryString)
+			.then((data) => { return data; })
+			.catch((err) => { return err; });
 	},
 	id: (id) => {
 
