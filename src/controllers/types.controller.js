0dc7d1bfb89a78b0efcf251501f12b93104e7440
misspia <pia_leung@hotmail.com>
2017-10-28 13:24:21 -0400

    init types, all types

--- src/controllers/types.controller.js
+++ src/controllers/types.controller.js
@@ -2,7 +2,24 @@ const Types = require('../models/types.model.js');
 
 const Controller = {
 	all: (req, res) => {
-		res.send('Types endpoint');
+		Types.all()
+		.then((data) => res.send(data))
+		.catch((err) => res.send(err));
+	},
+	id: (req, res) => {
+		Types.id(req.params.id)
+		.then((data) => res.send(data))
+		.catch((err) => res.send(err));
+	},
+	effectivenessAll: (req, res) => {
+		Types.effectivenessAll()
+		.then((data) => res.send(data))
+		.catch((err) => res.send(err));
+	},
+	effectivenessId: (req, res) => {
+		Types.effectivenessId(req.params.id)
+		.then((data) => res.send(data))
+		.catch((err) => res.send(err));
 	}
 };
 
--- src/models/types.model.js
+++ src/models/types.model.js
@@ -1,5 +1,33 @@
-const Model = {
+const squel = require('squel');
+const query = require('../pgconnect.js');
 
+const tables = {
+	types: 'pokemon.types',
+	effectiveness: 'pokemon.type_efficacy',
+
+};
+
+
+const Model = {
+	all: () => {
+		const queryString = squel.select()
+			.from(tables.types)
+			.field(`${tables.types}.id`)
+			.field(`${tables.types}.identifier`)
+			.toString();
+		return query(queryString)
+			.then((data) => { return data; })
+			.catch((err) => { return err; });
+	},
+	id: (id) => {
+
+	},
+	effectivenessAll: () => {
+
+	},
+	effectivenessId: (id) => {
+
+	}
 };
 
 module.exports = Model;
