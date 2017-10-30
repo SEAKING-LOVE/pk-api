b8228d244f70b209a8d438024e29a52641ba28e6
misspia <pia_leung@hotmail.com>
2017-10-30 19:25:58 -0400

    can get this subquery to work :(

--- src/models/abilities.model.js
+++ src/models/abilities.model.js
@@ -7,17 +7,31 @@ const tables = {
 }
 const Model = {
 	all: () => {
+		// select ability_id, language_id, max(version_group_id)
+		// from ability_flavor_text 
+		// where language_id = '9' group by language_id, ability_id;
+		const subQuery = squel.select()
+				.from(tables.abilitiesDesc)
+				.field(`${tables.abilitiesDesc}.ability_id`)
+				.field(`${tables.abilitiesDesc}.language_id`)
+				.field(`MAX(${tables.abilitiesDesc}.version_group_id)`)
+				.where(`${tables.abilitiesDesc}.language_id = 9`)
+				.group(`${tables.abilitiesDesc}.language_id`)
+				.group(`${tables.abilitiesDesc}.ability_id`)
+				// .toString();
+
 		const queryString = squel.select()
-			.distinct(`${tables.abilitiesDesc}.flavor_text`)
 			.from(tables.abilities)
 			.field(`${tables.abilities}.id`)
 			.field(`${tables.abilities}.identifier`)
 			.field(`${tables.abilitiesDesc}.flavor_text`)
-			.distinct() 	// figure out how to get distinc to work :S
-			.join(tables.abilitiesDesc, null, `${tables.abilities}.id = ${tables.abilitiesDesc}.ability_id`)
+			
+			// .join(tables.abilitiesDesc, null, `${tables.abilities}.id = ${tables.abilitiesDesc}.ability_id`)
+			.join(subQuery, null, `${tables.abilities}.id = ${tables.abilitiesDesc}.ability_id`)
 			.where(`${tables.abilitiesDesc}.language_id = 9`)
 			.order(`${tables.abilities}.identifier`)
 			.toString();
+		console.log(queryString);
 		return query(queryString)
 			.then((data) => { return data; })
 			.catch((err) => { return err; });
