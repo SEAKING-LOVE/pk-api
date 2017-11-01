02e97b5dd199c0b7a41a8bc91f504db480e55c98
misspia <pia_leung@hotmail.com>
2017-10-31 21:29:41 -0400

    clean up commented out work in abilities model

--- src/models/abilities.model.js
+++ src/models/abilities.model.js
@@ -7,9 +7,6 @@ const tables = {
 }
 const Model = {
 	all: () => {
-		// select ability_id, language_id, max(version_group_id)
-		// from ability_flavor_text 
-		// where language_id = '9' group by language_id, ability_id;
 		const subQuery = squel.select()
 				.from(tables.abilitiesDesc)
 				.field(`${tables.abilitiesDesc}.ability_id`)
@@ -18,20 +15,14 @@ const Model = {
 				.where(`${tables.abilitiesDesc}.language_id = 9`)
 				.group(`${tables.abilitiesDesc}.language_id`)
 				.group(`${tables.abilitiesDesc}.ability_id`)
-				//.toString();
 
 		const queryString = squel.select()
 			.from(tables.abilities)
 			.field(`${tables.abilities}.id`)
 			.field(`${tables.abilities}.identifier`)
-			//.field(`${tables.abilitiesDesc}.flavor_text`)
-			
-			// .join(tables.abilitiesDesc, null, `${tables.abilities}.id = ${tables.abilitiesDesc}.ability_id`)
 			.join(subQuery, "subQuery", `${tables.abilities}.id = subQuery.ability_id`)
-			//.where(`${tables.abilitiesDesc}.language_id = 9`)
 			.order(`${tables.abilities}.identifier`)
-			// .toString();
-		// console.log(queryString.toString());
+
 		return query(queryString)
 			.then((data) => { return data; })
 			.catch((err) => { return err; });
@@ -55,11 +46,8 @@ const Model = {
 			.field(`${tables.abilities}.identifier`)
 			.field(`subQuery.flavor_text`)
 			.join(subQuery, "subQuery", `${tables.abilities}.id = subQuery.ability_id`)
-			//.where(`${tables.abilitiesDesc}.language_id = 9`)
 			.where(`${tables.abilities}.id = ${id}`)
-			//.order(`${tables.abilities}.identifier`)
-			//.toString();
-		console.log(subQuery.toString());
+			.toString();
 		return query(queryString)
 			.then((data) => { return data; })
 			.catch((err) => { return err; });
