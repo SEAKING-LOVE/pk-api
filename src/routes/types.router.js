e6a68c9be3c4b049208428b5aece53ce21a12475
misspia <pia_leung@hotmail.com>
2017-11-02 14:18:54 -0400

    move list of pokemon by type to types endpoint

--- src/routes/types.router.js
+++ src/routes/types.router.js
@@ -5,5 +5,6 @@ router.get('/', Types.all);
 router.get('/:id', Types.id);
 router.get('/effectiveness/', Types.effectivenessAll);
 router.get('/effectiveness/:id', Types.effectivenessId);
+router.get('/pokemon/:id', Types.pokemon);
 
 module.exports = router;
