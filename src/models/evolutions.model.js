58d1665bb698c919984e2c3d25ef7178668d8d0f
misspia <pia_leung@hotmail.com>
2017-11-08 21:57:00 -0500

    add species table, add notes for gettign baby id

--- src/models/evolutions.model.js
+++ src/models/evolutions.model.js
@@ -1,5 +1,27 @@
-const Model = {
+const squel = require('squel');
 
+const tables = {
+	evo: 'pokemon.pokemon_evolution',
+	trigger: 'pokemon.evolution_trigger',
+	pk: 'pokemon.pokemon',
+	species: 'pokemon_species'
+};
+
+
+const Model = {
+	id: (id) => {
+		// group by evolution_chain_id and return id of pokemon where
+		// evolves_from_species_id = NULL because that should be the baby form
+		// https://github.com/SEAKING-LOVE/pk-api/blob/master/data/pokemon_species.csv
+		// DOCS: http://veekun.github.io/pokedex/main-tables.html#dex-table-pokemonspecies
+		const queryString = squel.select()
+			.field(`${tables.species}.id`)
+			.where()
+			.from(tables.evo);
+		return query(queryString)
+			.then((data) => { return data; })
+			.catch((err) => { return err; });
+	}
 };
 
 module.exports = Model;
