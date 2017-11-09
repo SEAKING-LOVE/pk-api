1c516f7c128b0f0a1b39c940ed673da2990df985
misspia <pia_leung@hotmail.com>
2017-11-08 21:43:56 -0500

    init evos

--- src/controllers/evolutions.controller.js
+++ src/controllers/evolutions.controller.js
@@ -2,7 +2,14 @@ const Evolutions = require('../models/evolutions.model.js');
 
 const Controller = {
 	all: (req, res) => {
-		res.send('evolutions endpoint');
+		Evolutions.all()
+		.then((data) => res.send(data))
+		.catch((err) => res.send(err));
+	},
+	id: (req, res) => {
+		Evolutions.id(req.params.id)
+		.then((data) => res.send(data))
+		.catch((err) => res.send(err));
 	}
 };
 
--- src/routes/evolutions.router.js
+++ src/routes/evolutions.router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const Evolutions = require('../controllers/evolutions.controller.js');
 
-router.get('/', Evolutions.all);
+// router.get('/', Evolutions.all);
+router.get('/:id', Evolutions.id);
 
 module.exports = router;
