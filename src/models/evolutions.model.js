d852e62afd5b2bcce7caf0af2c64873fa3fdf9dd
Jason Manson-Hing <jason.mansonhing@gmail.com>
2017-11-09 01:18:37 -0800

    Groundwork for evos

--- src/models/evolutions.model.js
+++ src/models/evolutions.model.js
@@ -1,10 +1,11 @@
-const squel = require('squel');
+const squel = require('squel').useFlavour('postgres');
+const query = require('../pgconnect.js');
 
 const tables = {
 	evo: 'pokemon.pokemon_evolution',
 	trigger: 'pokemon.evolution_trigger',
 	pk: 'pokemon.pokemon',
-	species: 'pokemon_species'
+	species: 'pokemon.pokemon_species'
 };
 
 
@@ -14,12 +15,27 @@ const Model = {
 		// evolves_from_species_id = NULL because that should be the baby form
 		// https://github.com/SEAKING-LOVE/pk-api/blob/master/data/pokemon_species.csv
 		// DOCS: http://veekun.github.io/pokedex/main-tables.html#dex-table-pokemonspecies
-		const queryString = squel.select()
-			.field(`${tables.species}.id`)
-			.where()
-			.from(tables.evo);
-		return query(queryString)
-			.then((data) => { return data; })
+		
+		const evoChainQuery = squel.select()
+			.from(tables.species)
+			.field(`${tables.species}.evolution_chain_id`)
+			.where(`${tables.species}.id = ${id}`)
+
+		const pokemonSpeciesQuery = (evoChain) => {
+			return squel.select()
+			.from(tables.species)
+			.where(`${tables.species}.evolution_chain_id = ${evoChain}`)
+
+		} 
+
+		return query(evoChainQuery)
+			.then((targetSpecies) => {
+				return query(pokemonSpeciesQuery(targetSpecies['evolution_chain_id']))
+					.then((chainSpecices) => {
+						return chainSpecices
+					})
+					.catch((err) => { return err; })
+			})
 			.catch((err) => { return err; });
 	}
 };
