135521e5237b44d56417cd43700147894993cdc6
misspia <pia_leung@hotmail.com>
2017-11-10 20:46:11 -0500

    add headers to allow access from any domain

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+
+//CORS: allow web requests from any domain
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.get('/', (req, res) => res.send('api root'));
 
 const API_PATH = '/v1/';
